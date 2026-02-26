d6c374cdd9710c1462745f7ac3d56555fdc3f94c
Miharu Matsuura <miharu03291020@gmail.com>
2026-02-26 14:31:30 +0900

    メニュー一覧のヘッダーフッターをphp化

---
+++ inc/footer.php
@@ -0,0 +1,41 @@
+    <footer class="l-footer">
+        <div class="l-footer__title">
+            <img src="./img/logo_01.png" alt="ふくおか餃子FES">
+            <dl class="l-footer__relevant-parties-list">
+                <div class="c-footer__relevant-parties-list-item">
+                    <dt class="c-footer__term">主催</dt>
+                    <dd class="c-footer__details">ふくおか餃子FES実行委員会</dd>
+                </div>
+                <div class="c-footer__relevant-parties-list-item">
+                    <dt class="c-footer__term">協賛</dt>
+                    <dd class="c-footer__details">九州餃子部</dd>
+                </div>
+                <div class="c-footer__relevant-parties-list-item">
+                    <dt class="c-footer__term">制作協力</dt>
+                    <dd class="c-footer__details">創造社リカレントスクール&nbsp;福岡校</dd>
+                </div>
+            </dl>
+        </div>
+        <div class="l-footer__info">
+            <div class="l-footer__contact">
+                <p class="c-footer__contact-item">ふくおか餃子FES実行委員会</p>
+                <p class="c-footer__contact-item"><a href="tel:000-0000-0000">(tel:)&nbsp;000-0000-0000</a></p>
+                <p class="c-footer__contact-item">xxxxxxxxxxx@xxx.com</p>
+            </div>
+            <ul class="l-sns-list l-sns-list--footer">
+                <li class="c-sns-icon c-sns-icon--footer"><a href="#"><img src="./img/sns_icon_x.png"
+                            alt="福岡餃子フェス 公式X"></a>
+                </li>
+                <li class="c-sns-icon c-sns-icon--footer"><a href="#"><img src="./img/sns_icon_instagram.png"
+                            alt="福岡餃子フェス 公式Instagram"></a>
+                </li>
+                <li class="c-sns-icon c-sns-icon--footer"><a href="#"><img src="./img/sns_icon_line.png"
+                            alt="福岡餃子フェス 公式LINE"></a>
+                </li>
+            </ul>
+        </div>
+        <a href="./privacy.html" class="c-privacy">個人情報保護方針</a>
+        <small class="c-copyright">
+            &copy;Copyright&nbsp;2025&nbsp;ふくおか餃子FES実行委員会
+        </small>
+    </footer>
