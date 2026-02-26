2dbef3818971105eeae2fc4a6c8e17c7f87cd79b
Takahisa Nagata <nagata.takahisa19@gmail.com>
2026-02-26 15:38:20 +0900

    質問カテゴリDBの管理画面追加

---
+++ admin/admin_faq_category.php
@@ -0,0 +1,56 @@
+<?php
+require_once __DIR__ . "/../functions/function.php";
+
+$db = db_connect();
+$sql = "SELECT * FROM faq_category";
+$stmt = $db->prepare($sql);
+$stmt->execute();
+
+$datas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <title>チーム王将 | 質問カテゴリDB管理画面</title>
+</head>
+
+<body>
+    <?php include('admin-header.php');  ?>
+
+    <main role="main" class="container" style="padding:60px 15px 0">
+        <h1 class="my-5">質問カテゴリDB管理画面</h1>
+        <a href="#">
+            <p>質問カテゴリDBの新規登録はこちら</p>
+        </a>
+        <table class="table">
+            <thead>
+                <tr class="table-primary">
+                    <th>id</th>
+                    <th>カテゴリ名</th>
+                    <th>リンクID</th>
+                    <th>登録日時</th>
+                    <th>操作</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($datas as $data): ?>
+                    <tr>
+                        <td><?php echo $data["id"]; ?></td>
+                        <td><?php echo $data["category"]; ?></td>
+                        <td><?php echo $data["link_id"]; ?></td>
+                        <td><?php echo $data["create_date"]; ?></td>
+                        <td><button type="button" class="btn btn-primary mx-1">詳細</button><button type="button" class="btn btn-secondary mx-1">編集</button><button type="button" class="btn btn-danger mx-1">削除</button></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </main>
+</body>
+
+</html>
