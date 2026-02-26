cb09c2c0a563b8f86919f4356b81ad823124ffb6
Takahisa Nagata <nagata.takahisa19@gmail.com>
2026-02-26 15:47:33 +0900

    お知らせDBの管理画面を追加

---
+++ admin/admin_info.php
@@ -0,0 +1,62 @@
+<?php
+require_once __DIR__ . "/../functions/function.php";
+
+$db = db_connect();
+$sql = "SELECT * FROM info";
+$stmt = $db->prepare($sql);
+$stmt->execute();
+
+$datas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <title>チーム王将 | お知らせDB管理画面</title>
+</head>
+
+<body>
+    <?php include('admin-header.php');  ?>
+
+    <main role="main" class="container" style="padding:60px 15px 0">
+        <h1 class="my-5">お知らせDB管理画面</h1>
+        <a href="#">
+            <p>お知らせDBの新規登録はこちら</p>
+        </a>
+        <table class="table">
+            <thead>
+                <tr class="table-primary">
+                    <th>id</th>
+                    <th>タイトル</th>
+                    <th>本文</th>
+                    <th>お知らせ画像名</th>
+                    <th>公開日時</th>
+                    <th>更新日時</th>
+                    <th>登録日時</th>
+                    <th>操作</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($datas as $data): ?>
+                    <tr>
+                        <td><?php echo $data["id"]; ?></td>
+                        <td><?php echo $data["title"]; ?></td>
+                        <td><?php echo $data["body"]; ?></td>
+                        <td><?php echo $data["info_img"]; ?></td>
+                        <td><?php echo $data["public_date"]; ?></td>
+                        <td><?php echo $data["update_date"]; ?></td>
+                        <td><?php echo $data["created_date"]; ?></td>
+                        <td><button type="button" class="btn btn-primary mx-1">詳細</button><button type="button" class="btn btn-secondary mx-1">編集</button><button type="button" class="btn btn-danger mx-1">削除</button></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </main>
+</body>
+
+</html>
