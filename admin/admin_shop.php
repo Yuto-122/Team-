74dec852c00c5baa860d3ac5cbd5f8dff79ff8c1
Takahisa Nagata <nagata.takahisa19@gmail.com>
2026-02-26 15:33:03 +0900

    店舗DB管理画面を追加

--- admin/admin_shop.php
+++ admin/admin_shop.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . "/../functions/function.php";
 
 $db = db_connect();
-$sql = "SELECT menus.id AS menu_id, menus.name AS menu_name, shops.name AS shop_name FROM menus INNER JOIN shops ON menus.shop_id = shops.id";
+$sql = "SELECT id,name,kana,booth FROM shops";
 $stmt = $db->prepare($sql);
 $stmt->execute();
 
@@ -17,32 +17,34 @@ $datas = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
-    <title>チーム王将 | メニューDB管理画面</title>
+    <title>チーム王将 | 店舗DB管理画面</title>
 </head>
 
 <body>
     <?php include('admin-header.php');  ?>
 
     <main role="main" class="container" style="padding:60px 15px 0">
-        <h1 class="my-5">メニューDB管理画面</h1>
+        <h1 class="my-5">店舗DB管理画面</h1>
         <a href="#">
-            <p>メニューDBの新規登録はこちら</p>
+            <p>店舗DBの新規登録はこちら</p>
         </a>
         <table class="table">
-            <thead class="table-primary">
-                <tr>
+            <thead>
+                <tr class="table-primary">
                     <th>id</th>
-                    <th>商品名</th>
                     <th>店舗名</th>
+                    <th>読み仮名</th>
+                    <th>ブース番号</th>
                     <th>操作</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($datas as $data): ?>
                     <tr>
-                        <td><?php echo $data["menu_id"]; ?></td>
-                        <td><?php echo $data["menu_name"]; ?></td>
-                        <td><?php echo $data["shop_name"]; ?></td>
+                        <td><?php echo $data["id"]; ?></td>
+                        <td><?php echo $data["name"]; ?></td>
+                        <td><?php echo $data["kana"]; ?></td>
+                        <td><?php echo $data["booth"]; ?></td>
                         <td><button type="button" class="btn btn-primary mx-1">詳細</button><button type="button" class="btn btn-secondary mx-1">編集</button><button type="button" class="btn btn-danger mx-1">削除</button></td>
                     </tr>
                 <?php endforeach; ?>
