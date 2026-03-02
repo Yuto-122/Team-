de91239347f8d0ff4e8f12401e0798198d5ee8d1
Miharu Matsuura <miharu03291020@gmail.com>
2026-03-02 17:57:51 +0900

    admin_shop途中

---
+++ admin/shop_add_do.php
@@ -0,0 +1,55 @@
+<?php
+require_once __DIR__ . "/../functions/function.php";
+
+if (!empty($_POST)) {
+    if (!empty($_POST["name"]) && !empty($_POST["booth"]) && !empty($_POST["description"])) {
+        $name = $_POST["name"];
+        $booth = $_POST["booth"];
+        $desc = $_POST["description"];
+        $kana = $_POST["kana"];
+
+        if (!empty($_POST["kana"])) {
+            // 店舗名の読み仮名チェック（全角かなカナ1文字以上）
+            if (!preg_match("/^[ぁ-んァ-ヴ¥s¥x20 ]{1,}$/", $kana)) {
+                // 上記に満たさないkanaだった場合
+                header("location:shop_add.php");
+                exit();
+            }
+        }
+
+        try {
+            $db = db_connect();
+
+            $sql = 'SELECT count(*) FROM shops WHERE booth=:booth';
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(":booth", $booth, PDO::PARAM_STR);
+            $stmt->execute();
+
+            // キーを連番で取り出す(FETCH_NUM)
+            $result = $stmt->fetch(PDO::FETCH_NUM);
+
+            if ($result[0] !== 0) {
+                // 0でない(1)時はすでにユーザー名が登録されている状態
+                //header("location:shop_add.php");
+                //todo：matsuura 登録失敗の際、画面にエラーメッセージ表示
+                exit('そのブースは登録済みです');
+            }
+
+            $sql_2 = "INSERT INTO shops(name,kana,booth,description,created_date,update_date) VALUE(:name,:kana,:booth,:description,now(),now())";
+            $stmt = $db->prepare($sql_2);
+            $stmt->bindParam(":name", $name, PDO::PARAM_STR);
+            $stmt->bindParam(":kana", $kana, PDO::PARAM_STR);
+            $stmt->bindParam(":booth", $booth, PDO::PARAM_STR);
+            $stmt->bindParam(":description", $desc, PDO::PARAM_STR);
+            $stmt->execute();
+        } catch (PDOException $e) {
+            // 失敗したら入力画面へ戻す
+            // TODO nagata-t: エラーメッセージを入れるか検討（余裕があったら）
+            header("location:shop_add.php");
+            exit('エラー: ' . $e->getMessage());
+        }
+    }
+}
+
+header("location:admin_shop.php");
+exit();
---
+++ admin/shop_edit.php
@@ -0,0 +1,87 @@
+<?php
+require_once __DIR__ . "/../functions/function.php";
+
+if (empty($_GET)) {
+    // GETが無かったら戻す
+    header("location:admin_shop.php");
+    exit();
+}
+
+$id = $_GET["id"];
+
+// DB接続
+try {
+    $db = db_connect();
+    $sql = "SELECT * FROM shops WHERE id=:id";
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $data = $stmt->fetch(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    exit('エラー: ' . $e->getMessage());
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <title>チーム王将 | 管理者ページ</title>
+</head>
+
+<body>
+    <?php include('admin-header.php');  ?>
+
+    <main role="main" class="container" style="padding:60px 15px 0">
+        <h1 class="my-5">店舗DB編集画面</h1>
+        <div class="mb-3">
+            <p><b>ID</b></p>
+            <p><?php echo $data["id"]; ?></p>
+            <p><b>店舗名</b></p>
+            <p><?php echo $data["name"]; ?></p>
+            <p><b>読み仮名</b></p>
+
+            <p><b>ブース番号</b></p>
+            <p><?php echo $data["booth"]; ?></p>
+            <p><b>店舗説明</b></p>
+            <p><?php echo $data["description"]; ?></p>
+            <p><b>更新日時</b></p>
+            <p><?php echo $data["update_date"]; ?></p>
+            <p><b>作成日時</b></p>
+            <p><?php echo $data["created_date"]; ?></p>
+
+            <form action="shop_edit_do.php" method="post" class="mb-3">
+                <div class="mb-3">
+                    <label for="name" class="form-label">店舗名</label>
+                    <input type="text" name="name" id="name" class="form-control" value="<?php echo $data["name"]; ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="kana" class="form-label">読み仮名</label>
+                    <input type="kana" name="kana" id="kana" class="form-control" value=" <?php echo (!empty($data["kana"])) ?  $data["kana"]  : ""; ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="booth" class="form-label">ブース番号</label>
+                    <input type="text" name="booth" id="booth" class="form-control" value="<?php echo $data["booth"]; ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="description" class="form-label">店舗説明</label>
+                    <input type="text" name="description" id="description" class="form-control" value="<?php echo $data["description"]; ?>">
+                </div>
+            </form>
+        </div>
+        <div class="mb-3">
+            <input type="hidden" name="id" value="<?php echo $data["id"]; ?>">
+            <button type="submit" class="btn btn-primary">登録</button>
+            <a href="admin_shop.php" class="btn btn-secondary">一覧に戻る</a>
+        </div>
+        </form>
+
+    </main>
+</body>
+
+</html>
