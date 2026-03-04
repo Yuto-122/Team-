8091ef2841ab8701a206927407336af4573f073f
Takahisa Nagata <nagata.takahisa19@gmail.com>
2026-03-04 16:03:01 +0900

    fix

--- admin/admin-header.php
+++ admin/admin-header.php
@@ -6,15 +6,12 @@
     }
     ?>
 
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top px-3">
+    <nav class="navbar navbar-expand navbar-dark bg-dark fixed-top px-3">
         <a class="navbar-brand" href="./index.php">管理者ページ</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <div class="collapse navbar-collapse">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item"><a class="nav-link" href="./logout.php">ログアウト</a></li>
+                <a class="nav-link" href="./logout.php">ログアウト</a></li>
             </ul>
         </div>
         <?php if (isset($_SESSION["name"])): ?>
